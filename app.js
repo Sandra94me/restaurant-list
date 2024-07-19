18f6bab15fdd3f3e5615e5f5a830e81626081024
sandra94me <minionsandra88@gmail.com>
2024-07-19 16:08:37 +0800

    build:route & controller setup

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/items')
 })
+
+app.get('/items',(req, res)=>{
+  res.send('listing items')
+
+})
+
+app.get('/item/:id',(req, res)=>{
+  const id = req.params.id
+  res.send(`read item :${id}`)
+})
+
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
